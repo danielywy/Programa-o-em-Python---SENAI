7c444577de3b20a6b38dd9a7c63cd0da0e49a346
danielywy <danielmonteiropp@gmail.com>
2025-02-27 21:36:42 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+n1 = float (input('insira seu numero'))
+n2 = float (input('insira seu numero'))
+
+print('resultado da soma:', n1+n2)
+print('resultado da multiplição:', n1*n2)
+print('resultado da subtração:', n1-n2)
+print('resultado da divisão:', n1//n2)
---
+++ teste2.py
@@ -0,0 +1,27 @@
+nome = input('digite algo')
+print('boa noite', nome)
+
+idade = input('digite seu nome')
+print('voce tem',idade,'anos de idade')
+#concatenar -> juntar com texto (lembra da vigula, ela e importante)
+
+#verificar qual o tipo de dado
+#type()
+
+#mostar algo no ambiente de testes
+#função - ação
+print(type("ola mundo, como vai voçê?"))
+#dado do tipo inteiro
+print(type (10))
+#dado do tipo Real
+print(type (10.5))
+#dado do tipo boleano
+print(type (True))
+#py tem uma calculadora interna top de mais slc ai compensa KKKKKKKKKKK
+
+print(10 - 4)
+#nao prestei atenção, mas ela escreveu isso e eu copiei
+n=10
+print(n)
+#descobri, toda vez que eu digitar 'n'ele manda um 10, muito foda sou genio
+#input vai inserir um dado (ao enviar uma mensagem/codigo)
